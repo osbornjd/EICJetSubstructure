e37d9c8bd56f18d629736b3beac32eed798227ae
Joe Osborn <osbornjd@ornl.gov>
2020-04-17 13:39:14 -0400

    add jet definition classes

--- analysisCode/src/EventLoop.cpp
+++ analysisCode/src/EventLoop.cpp
@@ -1,26 +1,30 @@
-
+/// Fastjet includes
 #include <fastjet/ClusterSequence.hh>
 #include <fastjet/Selector.hh>
 #include <fastjet/contrib/SoftDrop.hh>
-#include <iostream>
 
+/// Root includes
 #include <TFile.h>
 #include <TTree.h>
 #include <TSystem.h>
+
+/// EIC smear includes
 #include <eicsmear/erhic/EventPythia.h>
 #include <eicsmear/smear/EventS.h>
+
+/// Local includes
 #include "include/TruthEvent.h"
 #include "include/SmearedEvent.h"
 #include "include/JetDef.h"
+#include "include/SoftDropJetDef.h"
 
-using namespace std;
-using namespace fastjet;
+#include <iostream>
 
 int main()
 {
   TruthEvent event;
   SmearedEvent smearevent;
-JetDef jetdef;
-  
+  JetDef jetdef;
+  SoftDropJetDef sd(0.1, 2, 0.5);
 
 }
--- analysisCode/src/include/JetDef.h
+++ analysisCode/src/include/JetDef.h
@@ -1,18 +1,39 @@
 #ifndef JETDEF_H
 #define JETDEF_H
 
+#include <fastjet/ClusterSequence.hh>
+#include <fastjet/Selector.hh>
 
+#include <vector>
 
 class JetDef {
   
  public:
-  JetDef();
+  JetDef(){}
+  JetDef(fastjet::JetAlgorithm alg, double R)  
+    : m_R(R)
+    , m_jetAlg(alg)
+    , m_jetDef(alg, R)   
+  {}
   ~JetDef(){}
+  
+  void setRadius(float rad){m_R = rad;}
+  void setMinJetPt(float jetPt){m_minJetPt = jetPt;}
+  void setMinJetRapidity(float rap){m_absRapidity = rap;}
+  void setJetAlg(fastjet::JetAlgorithm alg){m_jetAlg = alg;}
+  void setJetDef(fastjet::JetDefinition def){m_jetDef = def;}
+  void setRecombScheme(fastjet::RecombinationScheme scheme){m_scheme = scheme;}
 
- private:
+ protected:
+  double m_R;
+  double m_minJetPt;
+  double m_absRapidity;
 
-
-
+  /// Fastjet jet definition
+  fastjet::JetAlgorithm m_jetAlg;
+  fastjet::JetDefinition m_jetDef;
+  /// Default to 4 vector e recombination scheme
+  fastjet::RecombinationScheme m_scheme = fastjet::RecombinationScheme::E_scheme;
 };
 
 
