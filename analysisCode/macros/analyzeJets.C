bb3f6c54af508cd863226cc958d27092eadf494d
Joe Osborn <osbornjd@ornl.gov>
2020-05-15 14:08:03 -0400

    add some analysis code

--- analysisCode/macros/analyzeJets.C
+++ analysisCode/macros/analyzeJets.C
@@ -4,7 +4,27 @@
 
 void analyzeJets()
 {
+  std::string filename = "pE275_pE18_minqsq9_breit.root";
+  infile = TFile::Open(filename.c_str());
 
+  /// If filename contains breit
+  if(filename.find("breit") != string::npos)
+    {
+
+      // Swap the q binning around, since breit frame specifically boosts
+      // q to be -pz
+      float dummybins[nq2bins+1];
+      for(int i = 0; i < nq2bins+1; i++)
+	{
+	  dummybins[i] = qbins[i];
+	}
+      
+      for(int i = 0; i < nq2bins+1; i++)
+	{
+	  qbins[i] = dummybins[nq2bins-i] * -1;
+	}
+    }
+  
   gROOT->ProcessLine(".L ../src/fastJetLinker.C+");
   setupTree();
 
@@ -27,6 +47,16 @@ void write()
   trueQ2pT->Write();
   truejetpteta->Write();
   truejetptphi->Write();
+  recojetptr->Write();
+  recojetptjt->Write();
+  recojetptz->Write();
+  recojetpteta->Write();
+  recojetptphi->Write();
+  recotruejetpt->Write();
+  recotruejetphi->Write();
+  recotruejeteta->Write();
+  recotruejetp->Write();
+  recotruejete->Write();
 
   outfile->Write();
 
@@ -36,16 +66,108 @@ void write()
 void instantiateHistos()
 {
   
-  truerecx = new TH2F("truerecx",";x_{true}; x_{rec}",nxbins, xbins, nxbins, xbins);
+  truerecx = new TH2F("truerecx",";x_{true}; x_{rec}",nxbins, xbins, 
+		      nxbins, xbins);
   truerecy = new TH2F("truerecy",";y_{true}; y_{rec}",100,0,1,100,0,1);
-  truerecq2 = new TH2F("truerecq2",";Q^{2}_{true} [GeV^{2}]; Q^{2}_{reco} [GeV^{2}]",nq2bins,qbins,nq2bins,qbins);
-  trueQ2x = new TH2F("trueq2x",";x_{true};Q^{2}_{true} [GeV^{2}]",nxbins,xbins,nq2bins,qbins);
+  truerecq2 = new TH2F("truerecq2",
+		       ";Q^{2}_{true} [GeV^{2}]; Q^{2}_{reco} [GeV^{2}]",
+		       nq2bins,qbins,nq2bins,qbins);
+  trueQ2x = new TH2F("trueq2x",";x_{true};Q^{2}_{true} [GeV^{2}]",
+		     nxbins,xbins,nq2bins,qbins);
   
-  trueQ2pT = new TH2F("trueq2pt",";Q_{true}^{2} [GeV^{2}]; p_{T}^{true} [GeV]",nq2bins,qbins,nptbins,ptbins);
-  truejetpteta = new TH2F("truejetpteta",";p_{T}^{true} [GeV]; #eta",20,4,24,50,-3,3);
-  truejetptphi = new TH2F("truejetptphi",";p_{T}^{true} [GeV]; #phi [rad]",20,4,24,50,-3.14159,3.14159);
+  trueQ2pT = new TH2F("trueq2pt",";Q_{true}^{2} [GeV^{2}]; p_{T}^{true} [GeV]",
+		      nq2bins,qbins,nptbins,ptbins);
+  truejetpteta = new TH2F("truejetpteta",";p_{T}^{true} [GeV]; #eta",
+			  30,4,34,50,-3,3);
+  truejetptphi = new TH2F("truejetptphi",";p_{T}^{true} [GeV]; #phi [rad]",
+			  30,4,34,50,-3.14159,3.14159);
+  recojetptz = new TH2F("recojetptz",";z_{reco};p_{T}^{jet,reco} [GeV]",
+			nzbins,zbins,nptbins,ptbins);
+  recojetptjt = new TH2F("recojetptjt",
+			 ";j_{T}^{reco} [GeV]; p_{T}^{jet,reco} [GeV]",
+			 njtbins,jtbins,nptbins,ptbins);
+  recojetptr = new TH2F("recojetptr",";r_{reco}; p_{T}^{jet,reco} [GeV]",
+			nrbins,rbins,nptbins,ptbins);
+  recojetpteta = new TH2F("recojetpteta", ";p_{T}^{jet,reco} [GeV]; #eta",
+			  30,4,34,50,-3,3);
+  recojetptphi = new TH2F("recojetptphi",";p_{T}^{jet,reco} [GeV]; #phi",
+			  30,4,34,50,-3.14159,3.14159);
+  recotruejetpt = new TH2F("recotruejetpt",";p_{T}^{jet,true} [GeV]; p_{T}^{jet,reco} [GeV]",
+			   nptbins, ptbins, nptbins, ptbins);
+  recotruejetphi = new TH2F("recotruejetphi",";#phi_{true} [rad];#phi_{reco} [rad]",
+			    200, -3.14159,3.14159, 200,-3.14159, 3.14159);
+  recotruejeteta = new TH2F("recotruejeteta",";#eta_{true}; #eta_{reco}",
+			    200,-3,3, 200,-3,3);
+  recotruejetp = new TH2F("recotruejetp",";p^{jet,true} [GeV];p^{jet,reco} [GeV]", npbins, pbins, npbins, pbins);
+  recotruejete = new TH2F("recotruejete",";E^{jet,true} [GeV]; E^{jet,reco} [GeV]", npbins, pbins, npbins, pbins);
+
+
 }
 
+void recoJetAnalysis(JetConstVec *recojets)
+{
+
+  for(int i = 0; i < recojets->size(); i++)
+    {
+      TLorentzVector jet;
+      jet = recojets->at(i).first;
+
+      float jetpt = jet.Pt();
+      recojetpteta->Fill(jetpt, jet.Eta());
+      recojetptphi->Fill(jetpt, jet.Phi());
+
+      TVector3 jet3;
+      jet3.SetXYZ(jet.Px(), jet.Py(), jet.Pz());
+
+      /// Iterate over constituents
+      for(int j = 0; j < recojets->at(i).second.size(); ++j)
+	{
+	  TLorentzVector con = recojets->at(i).second.at(j);
+	  TVector3 con3;
+	  con3.SetXYZ(con.Px(), con.Py(), con.Pz());
+	  TVector3 cross = jet3.Cross(con3);
+	  
+	  float z = jet3.Dot(con3) / (jet3.Mag2());
+	  float jt = cross.Mag() / jet3.Mag();
+	  float r = sqrt(pow(jet.Phi() - con.Phi(), 2) + pow(jet.Rapidity() - con.Rapidity(),2));
+
+	  recojetptz->Fill(z, jetpt);
+	  recojetptjt->Fill(jt, jetpt);
+	  recojetptr->Fill(r, jetpt);
+
+	}
+    }
+}
+
+double truthJetAnalysis(JetConstVec *truthjets)
+{
+
+  double jetpt = 0;
+  for(int jet = 0; jet < truthJets->size(); jet++)
+    {
+      TLorentzVector jetVec;
+      jetVec = truthJets->at(jet).first;
+      if(jetVec.Pt() > jetpt)
+	jetpt = jetVec.Pt();
+      
+      if(jetVec.Pt() < 4)
+	continue;
+      if(fabs(jetVec.Eta()) > 2)
+	continue;
+      
+      truejetptphi->Fill(jetVec.Pt(), jetVec.Phi());
+      truejetpteta->Fill(jetVec.Pt(), jetVec.Eta());
+      
+      for(int con = 0; con < truthJets->at(jet).second.size(); ++con)
+	{
+	  TLorentzVector constituent;
+	  constituent = truthJets->at(jet).second.at(con);
+	  
+	} 
+    }
+
+  return jetpt;
+}
 
 void loop()
 {
@@ -56,52 +178,49 @@ void loop()
 	std::cout << "Processed " << i << " events " << std::endl;
       jettree->GetEntry(i);
       
-      for(int jet = 0; jet < recoJets->size(); jet++)
-	{
+      recoJetAnalysis(recoJets);
 
-	}
+      float highestTruthJetPt = truthJetAnalysis(truthJets);
 
-      float jetpt = 0;
-      for(int jet = 0; jet < truthJets->size(); jet++)
-	{
-	  TLorentzVector jetVec;
-	  jetVec = truthJets->at(jet).first;
-	  if(jetVec.Pt() > jetpt)
-	    jetpt = jetVec.Pt();
-	  
-	  if(jetVec.Pt() < 4)
-	    continue;
-	  if(fabs(jetVec.Eta()) > 2)
-	    continue;
+      analyzeMatchedJets(matchedJets);
 
-	  truejetptphi->Fill(jetVec.Pt(), jetVec.Phi());
-	  truejetpteta->Fill(jetVec.Pt(), jetVec.Eta());
-
-	  for(int con = 0; con < truthJets->at(jet).second.size(); ++con)
-	    {
-	      TLorentzVector constituent;
-	      constituent = truthJets->at(jet).second.at(con);
-
-	    }
-
-	}
-      
+      /// Event level kinematics
       truerecx->Fill(truex,recx);
       truerecy->Fill(truey,recy);
       truerecq2->Fill(trueq2,recq2);
       trueQ2x->Fill(truex,trueq2);
-      trueQ2pT->Fill(trueq2,jetpt);
+  
+      trueQ2pT->Fill(truthExchangeBoson->Mag2(),highestTruthJetPt);
       
 
     }
 
 }
 
+void analyzeMatchedJets(MatchedJets *matchedjets)
+{
+  for(int i = 0; i < matchedjets->size(); i++)
+    {
+      JetConstPair truthJetConst = matchedjets->at(i).at(0);
+      JetConstPair recoJetConst = matchedJets->at(i).at(1);
 
+      TLorentzVector truthJet = truthJetConst.first;
+      TLorentzVector recoJet = recoJetConst.first;
+      TLorentzVectorVec truthConst = truthJetConst.second;
+      TLorentzVectorVec recoConst = recoJetConst.second;
+      
+      recotruejetpt->Fill(truthJet.Pt(), recoJet.Pt());
+      recotruejeteta->Fill(truthJet.Eta(), recoJet.Eta());
+      recotruejetphi->Fill(truthJet.Phi(), recoJet.Phi());
+      
+      recotruejetp->Fill(truthJet.P(), recoJet.P());
+      recotruejete->Fill(truthJet.E(), recoJet.E());
+    }
+
+}
 
 void setupTree()
 {
-  infile = TFile::Open("pE275_pE18_minqsq9_lab.root");
 
   jettree = (TTree*)infile->Get("jettree");
   jettree->SetBranchAddress("recx", &recx);
--- analysisCode/macros/analyzeJets.h
+++ analysisCode/macros/analyzeJets.h
@@ -1,51 +1,73 @@
-
-
 #include <TH1.h>
 #include <TH2.h>
 #include <TFile.h>
 #include <TTree.h>
 #include <TLorentzVector.h>
-
+#include <TVector3.h>
 #include <iostream>
 
 
 using TLorentzVectorVec = std::vector<TLorentzVector>;
 /// A JetConstPair is a pair of a jet 4 vector and vector of jet constituents
-using JetConstPair = std::pair<TLorentzVector, std::vector<TLorentzVector>>;
+using JetConstPair = std::pair<TLorentzVector, TLorentzVectorVec>;
 using JetConstVec = std::vector<JetConstPair>;
+using MatchedJets = std::vector<JetConstVec>;
 
 void setupTree();
 void instantiateHistos();
 void loop();
 void write();
 
+void recoJetAnalysis(JetConstVec *recojets);
+/// Returns highest truth jet pt in event
+double truthJetAnalysis(JetConstVec *truthjets);
+void analyzeMatchedJets(MatchedJets *matchedjets);
+
 TLorentzVector *truthExchangeBoson;
 JetConstVec *truthJets, *recoJets, *recoSDJets;
-std::vector<std::vector<JetConstPair>> *matchedJets, *matchedSDJets;
+MatchedJets *matchedJets, *matchedSDJets;
 double recx, recy, recq2, truey, truex, trueq2;
 
 TFile *infile, *outfile;
 TTree *jettree;
+
+TH2 *recojetpteta, *recojetptphi;
 TH2 *truerecx, *truerecy, *truerecq2;
 TH2 *trueQ2x, *trueQ2pT;
 TH2 *truejetpteta, *truejetptphi;
-
+TH2 *recojetptz, *recojetptjt, *recojetptr;
+TH2 *recotruejetpt, *recotruejeteta, *recotruejetphi, 
+  *recotruejetp, *recotruejete;
 
 const int nxbins = 41;
-const int nq2bins = 93;
+const int nq2bins = 101;
 const int nptbins = 14;
+const int npbins = 42;
+const int nzbins = 28;
+const int njtbins = 44;
+const int nrbins = 20;
+
+float pbins[npbins+1] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,24,26,28,30,32,34,36,38,40,44,48,52,56,60,70,80,90,100,120,140,160,180,200};
 
 float ptbins[nptbins+1]={4,5,6,7,8,9,10,11,12,14,16,18,20,24,30};
 
 float xbins[nxbins+1] = {0.0001,0.0002,0.0003,0.0004,0.0005,0.0006,0.0007,
 			 0.0008,0.0009,0.001,0.002,0.003,0.004,0.005,0.006,
-			 0.007,0.008,0.009,0.01,0.015,0.02,0.03,0.04,0.05,0.06,0.07,
-			 0.08,0.09,0.1,0.14,0.18,0.22,0.26,0.3,0.35,0.4,0.45,
-			 0.5,0.6,0.7,0.8,0.9};
+			 0.007,0.008,0.009,0.01,0.015,0.02,0.03,0.04,0.05,
+			 0.06,0.07, 0.08,0.09,0.1,0.14,0.18,0.22,0.26,0.3,
+			 0.35,0.4,0.45,0.5,0.6,0.7,0.8,0.9};
 
-float qbins[nq2bins+1] = {9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
+float qbins[nq2bins+1] = {1,2,3,4,5,6,7,8,
+			  9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
 			  26,27,28,29,30,31,32,33,34,35,36,37,38,40,42,44,46,
 			  48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,
 			  85,90,95,100,110,120,130,140,150,160,170,180,190,200,
-			  210,220,230,240,250,260,270,280,290,300,310,320,330,340,
-			  350,375,400,425,450,475,500,525,550,575,600,625,650,675,700};
+			  210,220,230,240,250,260,270,280,290,300,310,320,330,
+			  340, 350,375,400,425,450,475,500,525,550,575,600,625,
+			  650,675,700};
+
+float zbins[nzbins+1] = {0.0001,0.0005,0.001,0.002,0.004,0.006,0.008,0.01,0.015,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.125,0.15,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1};
+
+float jtbins[njtbins+1] = {0.0001,0.0005,0.0007,0.001,0.002,0.003,0.004,0.005,0.007,0.009,0.01,0.02,0.03,0.04,0.05,0.1,0.15,0.2,0.25,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.2,1.4,1.6,1.8,2,2.2,2.4,2.6,2.8,3,3.5,4,4.5,5,6,7,8,9};
+
+float rbins[nrbins+1] = {0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1};
